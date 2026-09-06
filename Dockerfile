89bd2e6fa131dbcbc3adcdf91001d40c4fe5bba4
qSaire <jervex@mail.ru>
2026-09-06 17:46:57 +0500

    Подключил Docker с БД и написал взаимодействие с консолью

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,11 @@
-FROM openjdk:17-jdk-slim
+FROM eclipse-temurin:17-jdk
 
 WORKDIR /app
 
-COPY pom.xml ./
+FROM maven:3.9.6-eclipse-temurin-17 AS build
+WORKDIR /app
+COPY pom.xml .
 RUN mvn dependency:go-offline
+
 COPY src ./src
 RUN mvn package
--- src/main/java/org/example/userservice/Main.java
+++ src/main/java/org/example/userservice/Main.java
@@ -1,7 +1,26 @@
 package org.example.userservice;
 
+import org.example.userservice.console.ConsoleMenu;
+import org.example.userservice.database.dao.UserDao;
+import org.example.userservice.database.dao.impl.UserDaoImpl;
+import org.example.userservice.mapper.UserMapper;
+import org.example.userservice.service.UserService;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("User Service");
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+        try {
+            UserDao userDao = new UserDaoImpl(sessionFactory);
+
+            UserService userService = new UserService(userDao, new UserMapper());
+            ConsoleMenu consoleMenu = new ConsoleMenu(userService);
+            consoleMenu.run();
+        } finally {
+            sessionFactory.close();
+        }
     }
 }
